355766a36599c2f54699b2692976cf40e8d9c1f5
Assuliya <t.nesterchuk@mail.ru>
2016-08-26 12:01:36 +0300

    models, pages, validations, all the basic functionality

--- apps/travel_plan/models.py
+++ apps/travel_plan/models.py
@@ -1,5 +1,82 @@
 from __future__ import unicode_literals
-
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
+import bcrypt, re
+from datetime import datetime
 
-# Create your models here.
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+PASS_REGEX = re.compile(r'\d.*[A-Z]|[A-Z].*\d')
+
+class UserManager(models.Manager):
+    def validateReg(self, request):
+        errors = []
+        if len(request.POST['name']) < 3:
+            errors.append('Name can not be less than 3 characters')
+        elif not all(x.isalpha() or x.isspace() for x in request.POST['name']):
+            errors.append('Name should only contain letters')
+        if len(request.POST['alias']) < 3:
+            errors.append('Username can not be less than 3 characters')
+        try:
+            user = User.objects.get(alias = request.POST['alias'])
+            errors.append('This alias is already being used')
+        except ObjectDoesNotExist:
+            pass
+        if len(errors) > 0:
+            return (False, errors)
+        return (True, errors)
+
+    def validateRegPass(self, request):
+        errors = []
+        if len(request.POST['password']) < 1:
+            errors.append('Password can not be empty')
+        elif len(request.POST['password']) < 8:
+            errors.append('Password should be more than 7 characters')
+        elif not PASS_REGEX.match(request.POST['password']):
+            errors.append('Password should contain at least one apper case letter and one number')
+        if request.POST['password'] != request.POST['repeat']:
+            errors.append('Password repeat did not match the password')
+        if len(errors) > 0:
+            return (False, errors)
+        return (True, errors)
+
+    def validateLogin(self, request):
+        from bcrypt import hashpw, gensalt
+        errors = []
+        try:
+	        user = self.get(alias=request.POST['alias'])
+	        password = user.pw_hash.encode()
+	        loginpass = request.POST['password'].encode()
+	        if hashpw(loginpass, password) == password:
+	            return (True, user)
+	        else:
+	            errors.append("Sorry, no password match. Please try again.")
+	            return (False, errors)
+        except ObjectDoesNotExist:
+            pass
+        errors.append("Sorry, no username found. Please try again.")
+        return (False, errors)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    alias = models.CharField(max_length=45)
+    pw_hash = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    objects = models.Manager()
+    manager = UserManager()
+
+class Travel(models.Model):
+      destination = models.CharField(max_length=255)
+      plan = models.TextField(max_length=500)
+      start = models.DateField()
+      end = models.DateField()
+      user_id = models.ForeignKey(User, related_name='travel_create')
+      created_at = models.DateTimeField(auto_now_add=True)
+      updated_at = models.DateTimeField(auto_now=True)
+
+class Join(models.Model):
+      travel_id = models.ForeignKey(Travel, related_name='join_travel')
+      user_id = models.ForeignKey(User, related_name='join_user')
+      created_at = models.DateTimeField(auto_now_add=True)
+      updated_at = models.DateTimeField(auto_now=True)
--- apps/travel_plan/views.py
+++ apps/travel_plan/views.py
@@ -3,18 +3,96 @@ from django.urls import reverse
 from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 # from django.db.models import Count
-# from models import User, Poke
+from models import User, Travel, Join
 import bcrypt
-# from datetime import date
+from datetime import date
 
 def index(request):
     return render(request, 'travel_plan/index.html')
 
 def user(request):
-    return render(request, 'travel_plan/user.html')
+    user = User.objects.get(id = request.session['user'])
+    other = Travel.objects.all().order_by('start')
+    travels = Travel.objects.filter(user_id = request.session['user'])
 
-def travel(request):
-    return render(request, 'travel_plan/travel.html')
+    joins = Join.objects.filter(user_id = request.session['user'])
+
+    
+
+    context = {'travels':travels, 'user':user, 'other':other, 'joins': joins}
+    return render(request, 'travel_plan/user.html', context)
+
+def travel(request, travel_id):
+    travel = Travel.objects.get(id = travel_id)
+    context = {'travel':travel}
+    return render(request, 'travel_plan/travel.html', context)
 
 def add(request):
-    return render(request, 'travel_plan/add.html')
+    today = date.today()
+    format_time = today.strftime('%Y-%m-%d')
+    context = {'time':format_time}
+    return render(request, 'travel_plan/add.html', context)
+
+
+
+
+def register_process(request):
+    result = User.manager.validateReg(request)
+    resultPass = User.manager.validateRegPass(request)
+    if result[0] == False or resultPass[0] == False:
+        errors = result[1]+resultPass[1]
+        print_messages(request, errors)
+        return redirect(reverse('index'))
+    pw_hash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+    user = User.manager.create(name=request.POST['name'], alias=request.POST['alias'], pw_hash=pw_hash)
+    return log_user_in(request, user)
+
+def login_process(request):
+    result = User.manager.validateLogin(request)
+    if result[0] == False:
+        print_messages(request, result[1])
+        return redirect(reverse('index'))
+    return log_user_in(request, result[1])
+
+def print_messages(request, message_list):
+    for message in message_list:
+        messages.add_message(request, messages.ERROR, message)
+
+def log_user_in(request, user):
+    request.session['user'] = user.id
+    return redirect(reverse('user'))
+
+def logout(request):
+    user = User.manager.get(id=request.session['user'])
+    request.session.pop('user')
+    return redirect(reverse('index'))
+
+def add_travel(request):
+    errors = []
+    print request.POST['end']
+    if len(request.POST['destination']) < 1:
+        errors.append('Destination can not be empty')
+    if len(request.POST['plan']) < 1:
+        errors.append('Plan can not be empty')
+    if len(request.POST['start']) < 1:
+        errors.append('Travel Date From can not be empty')
+    if len(request.POST['end']) < 1:
+        errors.append('Travel Date To can not be empty')
+    if request.POST['end'] < request.POST['start']:
+        errors.append('Travel Date To can not be earlier than Travel Date From')
+    if len(errors) > 0:
+        print errors
+        print_messages(request, errors)
+        return redirect(reverse('add'))
+    creator = User.objects.get(id = request.session['user'])
+    travel = Travel.objects.create(user_id_join = creator, user_id = creator, destination=request.POST['destination'], plan=request.POST['plan'], start=request.POST['start'], end=request.POST['end'])
+    return redirect(reverse('user'))
+
+def join(request, travel_id):
+
+    user = User.objects.get(id = request.session['user'])
+    travel = User.objects.get(id = request.session['user'])
+
+    join = Join.objects.create(travel_id = travel, user_id = user)
+
+    return redirect(reverse('user'))
